ff278c6e3c962dbfa8dd831769720bb58c8f0e0d
YiGuMoYan <3194775246@qq.com>
2022-12-29 23:02:00 +0800

    asideHead ok

--- src/components/aside/head/HeadComponents.vue
+++ src/components/aside/head/HeadComponents.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="head">
     <!-- 头像 -->
-    <img src="../../assets/Head.jpg" alt="head">
+    <img src="../../../assets/Head.jpg" alt="head">
     <!-- 标签块 -->
     <div class="tagBlock">
-      <span><HeadTagComponent name="博客" num:0/></span>
-      <span class="tagMargin"><HeadTagComponent name="标签" num:0/></span>
+      <span><HeadTagComponent name="博客" :num="markdownListNum"/></span>
+      <span class="tagMargin"><HeadTagComponent name="标签" :num="tagListNum"/></span>
     </div>
     <!-- 标语 -->
     <div class="slogan">{{ slogan }}</div>
@@ -16,11 +16,21 @@
 </template>
 
 <script>
-import HeadTagComponent from '@/components/head/HeadTagComponent.vue'
+import HeadTagComponent from '@/components/aside/head/HeadTagComponent.vue'
 
 export default {
   name: 'HeadComponents',
   components: { HeadTagComponent },
+  props: {
+    markdownListNum: {
+      type: Number,
+      default: 0
+    },
+    tagListNum: {
+      type: Number,
+      default: 0
+    }
+  },
   data () {
     return {
       slogan: '很高兴与您在这里相遇'
@@ -32,7 +42,7 @@ export default {
 <style lang="less" scoped>
 .head {
   background-color: #101f30;
-  width: 300px;
+  width: 100%;
   height: 500px;
   text-align: center;
   color: aliceblue;
