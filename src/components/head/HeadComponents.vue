442210bf556f22edaa2b31a9874e34cac1b33830
YiGuMoYan <3194775246@qq.com>
2022-12-29 21:27:49 +0800

    fix

---
+++ src/components/head/HeadComponents.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="head">
+    <!-- 头像 -->
+    <img src="../../assets/Head.jpg" alt="head">
+    <!-- 标签块 -->
+    <div class="tagBlock">
+      <span><HeadTagComponent name="博客" num:0/></span>
+      <span class="tagMargin"><HeadTagComponent name="标签" num:0/></span>
+    </div>
+    <!-- 标语 -->
+    <div class="slogan">{{ slogan }}</div>
+    <div class="connect">
+      <router-link class="link" to="connect">联系我们</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeadTagComponent from '@/components/head/HeadTagComponent.vue'
+
+export default {
+  name: 'HeadComponents',
+  components: { HeadTagComponent },
+  data () {
+    return {
+      slogan: '很高兴与您在这里相遇'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.head {
+  background-color: #101f30;
+  width: 300px;
+  height: 500px;
+  text-align: center;
+  color: aliceblue;
+  border-radius: 20px;
+
+  img {
+    margin-top: 30px;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    transition: all .5s;
+  }
+
+  img:hover {
+    transform: rotate(360deg);
+  }
+
+  .tagBlock {
+    margin-top: 30px;
+  }
+
+  .tagMargin {
+    margin-left: 20px;
+  }
+
+  .slogan {
+    margin-top: 30px;
+    font-size: 23px;
+    transition: all .3s;
+  }
+
+  .slogan:hover {
+    font-size: 25px;
+  }
+
+  .connect {
+    margin-top: 30px;
+    font-size: 15px;
+
+    .link {
+      color: aliceblue;
+      text-decoration: none;
+      transition: all .3s;
+    }
+
+    .link:hover {
+      color: #49B1F5;
+      font-size: 18px;
+    }
+  }
+}
+
+span {
+  display: inline-block;
+}
+</style>
---
+++ src/components/head/HeadTagComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="headTag">
+    <div class="name">{{name}}</div>
+    <div class="num">{{num}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeadTagComponent',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    num: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.headTag {
+  color: aliceblue;
+  width: 70px;
+  height: 100px;
+  transition: all .5s;
+  .name {
+    font-size: 20px;
+  }
+  .num {
+    font-size: 25px;
+    margin-top: 20px;
+  }
+}
+
+.headTag:hover {
+  color: #49B1F5;
+}
+</style>
