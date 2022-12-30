923d921026a075a3379032b9d201f082a3a0e46b
YiGuMoYan <3194775246@qq.com>
2022-12-30 22:11:54 +0800

    add v-md-editor

--- src/main.js
+++ src/main.js
@@ -7,9 +7,27 @@ import '@/assets/css/global.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import VMdPreview from '@kangc/v-md-editor/lib/preview'
+import '@kangc/v-md-editor/lib/style/preview.css'
+
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
+
+import Prism from 'prismjs'
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+
+VMdPreview.use(vuepressTheme, {
+  Prism,
+  extend (md) {
+    // md为 markdown-it 实例，可以在此处进行修改配置,并使用 plugin 进行语法扩展
+    // md.set(option).use(plugin);
+  }
+})
+
+Vue.use(VMdPreview)
 
 new Vue({
   router,
